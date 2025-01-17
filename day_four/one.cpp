02bdaac67805766aa37a05b315c03abf9bb1434d
NithinKonda <nithinkonda142@gmail.com>
2025-01-18 00:43:23 +0530

    feat: Implement grid traversal logic to count specific patterns in one.cpp

--- day_four/one.cpp
+++ day_four/one.cpp
@@ -18,10 +18,41 @@ int main() {
             return 1;
         }
     }
+    int answer =0;
 
 
+    int W = a[0].length();
 
-    
+    auto inside = [&](int row, int col) {
+        return 0 <= row && row < H && 0 <= col && col <W;
+    };
+
+    for(int row=0;row<H;row++) {
+        for(int col=0;col<W;col++) {
+            if(a[row][col] == 'X') {
+                for(int drow=-1;drow<=1;drow++) {
+                    for(int dcol=-1;dcol<=1;dcol++) {
+                        if(drow == 0 && dcol == 0) continue;
+                }
+                bool yes = true;
+
+                for(int i=0;i<4;i++) {
+                    int r2= row+drow*i;
+                    int c2= col+dcol*i;
+                    if(inside(r2,c2) && a[r2][c2] == TEMP[i])
+                    {
+
+                    }
+                    else{
+                        yes = false;
+                        break;
+                    }
+                }
+                answer += yes;
+            }
+        }
+    }
+
 
     // for (const string& row : a) {
     //     cout << row << endl;
