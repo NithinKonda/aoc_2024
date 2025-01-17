79ed3c7c36e1d82190fe6e0df07445d9b309b27a
NithinKonda <nithinkonda142@gmail.com>
2025-01-18 00:13:05 +0530

    refactor: Simplify logic and improve readability in two.cpp

--- day_three/two.cpp
+++ day_three/two.cpp
@@ -18,68 +18,50 @@ int main() {
 
 
 
-    int answer = 0;
-    
-    int n = (int) s.size();
 
 
-
-
-    auto getNumber = [&](int &i){
-        int x=0;
-        while (x<1000 && isdigit(s[i])){
-
-            x = 10*x+(s[i] - '0');
-            i++;
-        }
-
-        if( 1<=x && x<=999) {
-            return x;
-        }
-        return -1;
-        
-        
-    };
-
-
-    bool enable = true;
-
-    for(int i=0;i<n-7;i++) {
-
-
-        if (s.substr(i,4) == "do()") {
-            enable= true;
-        }
-        if(s.substr(i,7)=="don\'t"){
-            enable = false;
-        }
-
-        if(enable && s[i] == 'm'){
-            if(s[i+1]=='u' && s[i+2] == 'l' && s[i+3]=='(')
-            {
-                i+=4;
-
-
-                int x= getNumber(i);
-                if(s[i] == ',')
-                {
-                    i+=1;
-                    int y = getNumber(i);
-
-
-                    if(s[i]==')'){
-                        if(x!=-1 && y!=-1) {
-                            answer+=x*y;
-                        }
-                    }
-                }
-            }
-        }
-         
-    }
-
-
-         cout<<answer<<endl;
-
-
+int n = (int) s.length();
+	
+	auto getNumber = [&](int& i) {
+		int x = 0;
+		while (x < 1000 && isdigit(s[i])) {
+			x = 10 * x + (s[i] - '0');
+			i++;
+		}
+		if (1 <= x && x <= 999) {
+			return x;
+		}
+		return -1;
+	};
+	
+	bool enable = true;
+	int answer = 0;
+	for (int i = 0; i < n - 7; i++) {
+		if (s.substr(i, 4) == "do()") {
+			// cout << "do\n";
+			enable = true;
+		}
+		if (s.substr(i, 7) == "don\'t()") {
+			// cout << "do not\n";
+			enable = false;
+		}
+		if (enable && s[i] == 'm') {
+			if (s[i+1] == 'u' && s[i+2] == 'l' && s[i+3] == '(') {
+				i += 4;
+				int x = getNumber(i);
+				// cout << i << " " << x << endl;
+				if (s[i] == ',') {
+					i += 1;
+					int y = getNumber(i);
+					// cout << ">> " << i << " " << x << " " << y << endl;
+					if (s[i] == ')') {
+						if (x != -1 && y != -1) {
+							answer += x * y;
+						}
+					}
+				}
+			}
+		}
+	}
+	printf("%d\n", answer);
 }
