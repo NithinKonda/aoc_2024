37f3637481fdfa48852ce3aaafe6e126d76b7407
NithinKonda <nithinkonda142@gmail.com>
2025-01-18 00:46:52 +0530

    refactor: Improve code readability and structure in one.cpp

--- day_four/one.cpp
+++ day_four/one.cpp
@@ -3,61 +3,74 @@ using namespace std;
 #include <fstream>
 #include <vector>
 
-int main() {
+int main()
+{
     ifstream inputFile("input.txt");
-    if (!inputFile) {
+    if (!inputFile)
+    {
         cerr << "Unable to open file" << endl;
         return 1;
     }
 
     int H = 140;
     vector<string> a(H);
-    for (string& row : a) {
-        if (!(inputFile >> row)) {
+    for (string &row : a)
+    {
+        if (!(inputFile >> row))
+        {
             cerr << "Error reading from file" << endl;
             return 1;
         }
     }
-    int answer =0;
-
+    int answer = 0;
 
     int W = a[0].length();
-
-    auto inside = [&](int row, int col) {
-        return 0 <= row && row < H && 0 <= col && col <W;
+    const string TEMP = "XMAS";
+    auto inside = [&](int row, int col)
+    {
+        return 0 <= row && row < H && 0 <= col && col < W;
     };
 
-    for(int row=0;row<H;row++) {
-        for(int col=0;col<W;col++) {
-            if(a[row][col] == 'X') {
-                for(int drow=-1;drow<=1;drow++) {
-                    for(int dcol=-1;dcol<=1;dcol++) {
-                        if(drow == 0 && dcol == 0) continue;
-                }
-                bool yes = true;
-
-                for(int i=0;i<4;i++) {
-                    int r2= row+drow*i;
-                    int c2= col+dcol*i;
-                    if(inside(r2,c2) && a[r2][c2] == TEMP[i])
+    for (int row = 0; row < H; row++)
+    {
+        for (int col = 0; col < W; col++)
+        {
+            if (a[row][col] == 'X')
+            {
+                for (int drow = -1; drow <= 1; drow++)
+                {
+                    for (int dcol = -1; dcol <= 1; dcol++)
                     {
+                        if (drow == 0 && dcol == 0)
+                        {
+                            continue;
+                        }
+                        bool yes = true;
 
-                    }
-                    else{
-                        yes = false;
-                        break;
+                        for (int i = 0; i < 4; i++)
+                        {
+                            int r2 = row + drow * i;
+                            int c2 = col + dcol * i;
+                            if (inside(r2, c2) && a[r2][c2] == TEMP[i])
+                            {
+                            }
+                            else
+                            {
+                                yes = false;
+                                break;
+                            }
+                        }
+                        answer += yes;
                     }
                 }
-                answer += yes;
             }
         }
     }
+        cout<<answer<<endl;
+            // for (const string& row : a) {
+            //     cout << row << endl;
+            // }
+            // cout << a.size();
 
-
-    // for (const string& row : a) {
-    //     cout << row << endl;
-    // }
-    // cout << a.size();
-
-    return 0;
+            return 0;
 }
