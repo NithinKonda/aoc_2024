e2852bcbb70d80773227b42cc5277321153c65dd
NithinKonda <nithinkonda142@gmail.com>
2025-01-18 23:43:33 +0530

    feat: Refactor input processing and enhance pipe detection logic in one.cpp

--- day_five/one.cpp
+++ day_five/one.cpp
@@ -1,51 +1,36 @@
+#include <bits/stdc++.h>
 using namespace std;
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <cassert>
-#include <algorithm>
-#include <map>
 
 int main() {
-    string s;
-    map<int,vector<int>> edges;
-    while(getline(cin,s))
-    {
-        if(s.empty() || !isdigit(s[0])){
-            continue;
-        }
+	string s;
+	map<int, vector<int>> edges;
+	long long answer = 0;
+	while (getline(cin, s)) {
+		if (s.empty() || !isdigit(s[0])) {
+			continue;
+		}
+		int len = (int) s.length();
 
-        int len = (int) s.length();
-
-
-        vector<int> v;
-        bool is_pipe = false;
-        for(int i=0;i<len;i++)
-        {
-            if(isdigit(s[i]))
-            {
-                int x=0;
-                while(isdigit(s[i]))
-                {
-                    x=10*x+(s[i]-'0');
-                    i++;
-                }
-                v.push_back(x);
-
-                if (s[i] == '|')
-                {
-                    is_pipe = true;
-                }
-
-
-                assert(s[i] == ',' || s[i] == '|' || s[i]=='\n' || s[i]==0);
-            }
-        }
-        if (was_pipe) {
+		vector<int> v;
+		bool was_pipe = false;
+		for (int i = 0; i < len; i++) {
+			if (isdigit(s[i])) {
+				int x = 0;
+				while (isdigit(s[i])) {
+					x = 10 * x + (s[i] - '0');
+					i++;
+				}
+				v.push_back(x);
+				if (s[i] == '|') {
+					was_pipe = true;
+				}
+				assert(s[i] == ',' || s[i] == '|' || s[i] == '\n' || s[i] == 0);
+			}
+		}
+		if (was_pipe) {
 			edges[v[0]].push_back(v[1]);
 		}
-   else {
+		else {
 			set<int> earlier;
 			bool ok = true;
 			for (int i = 0; i < (int) v.size(); i++) {
