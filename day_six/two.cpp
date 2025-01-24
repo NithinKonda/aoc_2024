7ae2597d25e616c9ca44fde25f8f00e2e6cb1084
NithinKonda <nithinkonda142@gmail.com>
2025-01-24 22:48:03 +0530

    day_six2 init

---
+++ day_six/two.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<pair<int,int>> dirs = {{-1,0}, {0,1}, {1,0}, {0,-1}};
+
+
+int main() {
+	int H = 130;
+	vector<string> a(H);
+	for (int i = 0; i < H; i++) {
+		cin >> a[i];
+	}
+	int W = a[0].length();
+	
+	pair<int,int> start_me{-1, -1};
+	for (int row = 0; row < H; row++) {
+		for (int col = 0; col < W; col++) {
+			if (a[row][col] == '^') {
+				start_me = {row, col};
+				a[row][col] = '.';
+			}
+		}
+	}
+}
